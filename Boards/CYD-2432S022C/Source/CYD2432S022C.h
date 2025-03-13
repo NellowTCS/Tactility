b34ba11da50616219fdcb623280fb1f404a38854
NellowTCS <neeljaiswal23@gmail.com>
2025-03-13 15:43:07 -0600

    Update CYD2432S022C.h

--- Boards/CYD-2432S022C/Source/CYD2432S022C.h
+++ Boards/CYD-2432S022C/Source/CYD2432S022C.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #include <Tactility/hal/Configuration.h>
-
+#include "../../../sdkconfig.board.cyd-2432S022c"  
 // Capacitive touch version of the 2.2" yellow board
 extern const tt::hal::Configuration cyd_2432S022C_config;
