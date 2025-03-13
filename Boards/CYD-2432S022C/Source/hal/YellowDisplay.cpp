81cba2ed845ef1a324f90e9c82d8c313530d7cb7
NellowTCS <neeljaiswal23@gmail.com>
2025-03-13 17:28:05 -0600

    Update YellowDisplay.cpp

--- Boards/CYD-2432S022C/Source/hal/YellowDisplay.cpp
+++ Boards/CYD-2432S022C/Source/hal/YellowDisplay.cpp
@@ -23,20 +23,20 @@ std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
 
     // Replace Ili934xDisplay with St7789Display for I80 parallel interface
     auto configuration = std::make_unique<St7789Display::Configuration>(
-        LCD_I80_BUS_SUPPORT,
-        LCD_I80_BUS_WIDTH,
-        LCD_I80_BUS_CONFIG_CLK_SRC,
-        LCD_I80_BUS_CONFIG_DC,
-        LCD_I80_BUS_CONFIG_WR,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D8, // Adjust these GPIOs as necessary
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D9,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D10,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D11,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D12,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D13,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D14,
-        LCD_I80_BUS_CONFIG_DATA_GPIO_D15,
-        LCD_I80_BUS_CONFIG_BUS_WIDTH,
+        CONFIG_LCD_I80_BUS_SUPPORT,
+        CONFIG_LCD_I80_BUS_WIDTH,
+        CONFIG_LCD_I80_BUS_CONFIG_CLK_SRC,
+        CONFIG_LCD_I80_BUS_CONFIG_DC,
+        CONFIG_LCD_I80_BUS_CONFIG_WR,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D8, // Adjust these GPIOs as necessary
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D9,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D10,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D11,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D12,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D13,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D14,
+        CONFIG_LCD_I80_BUS_CONFIG_DATA_GPIO_D15,
+        CONFIG_LCD_I80_BUS_CONFIG_BUS_WIDTH,
         touch
     );
 
