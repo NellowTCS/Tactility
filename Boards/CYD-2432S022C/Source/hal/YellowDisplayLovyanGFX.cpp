d8e10681c58eb7c48253b202660590b39c983e37
NellowTCS <neeljaiswal23@gmail.com>
2025-09-14 01:45:01 +0000

    Accidentally forgot to change config to i8080 from st7789 example

--- Boards/CYD-2432S022C/Source/hal/YellowDisplayLovyanGFX.cpp
+++ Boards/CYD-2432S022C/Source/hal/YellowDisplayLovyanGFX.cpp
@@ -13,12 +13,6 @@ bool YellowDisplayLovyanGFX::createPanel(std::shared_ptr<lgfx::Panel_LCD>& outPa
     // Configure panel
     lgfx::Panel_ST7789::config_t panelCfg{};
     
-    // Interface config (pins)
-    panelCfg.pin_cs = CYD_2432S022C_LCD_PIN_CS;
-    panelCfg.pin_rst = CYD_2432S022C_LCD_PIN_RST;
-    panelCfg.pin_busy = -1;  // Not used
-    panelCfg.bus_shared = true;
-    
     // Panel config
     panelCfg.panel_width = CYD_2432S022C_LCD_HORIZONTAL_RESOLUTION;
     panelCfg.panel_height = CYD_2432S022C_LCD_VERTICAL_RESOLUTION;
@@ -32,12 +26,35 @@ bool YellowDisplayLovyanGFX::createPanel(std::shared_ptr<lgfx::Panel_LCD>& outPa
     panelCfg.rgb_order = false;
     panelCfg.dlen_16bit = false;
     panelCfg.bus_shared = true;
+    
+    // Interface pins (for i80)
+    panelCfg.pin_cs = CYD_2432S022C_LCD_PIN_CS;
+    panelCfg.pin_rst = CYD_2432S022C_LCD_PIN_RST;
+    panelCfg.pin_busy = -1;  // Not used
+    // Note: pin_rs/DC is set in the bus config
 
-    // Create and configure SPI bus
-    auto bus = new lgfx::Bus_SPI();
-    lgfx::Bus_SPI::config_t busCfg = {};
-    busCfg.spi_mode = 0;
-    busCfg.spi_3wire = false;
+    // Create and configure i80 (8-bit parallel) bus
+    auto bus = new lgfx::Bus_Parallel8();
+    lgfx::Bus_Parallel8::config_t busCfg = {};
+    
+    // Configure i80 bus pins
+    busCfg.pin_wr = CYD_2432S022C_LCD_PIN_WR;
+    busCfg.pin_rd = CYD_2432S022C_LCD_PIN_RD;
+    busCfg.pin_rs = CYD_2432S022C_LCD_PIN_DC;  // RS = DC (data/command) pin
+    
+    // Configure data pins
+    busCfg.pin_d0 = CYD_2432S022C_LCD_PIN_D0;
+    busCfg.pin_d1 = CYD_2432S022C_LCD_PIN_D1;
+    busCfg.pin_d2 = CYD_2432S022C_LCD_PIN_D2;
+    busCfg.pin_d3 = CYD_2432S022C_LCD_PIN_D3;
+    busCfg.pin_d4 = CYD_2432S022C_LCD_PIN_D4;
+    busCfg.pin_d5 = CYD_2432S022C_LCD_PIN_D5;
+    busCfg.pin_d6 = CYD_2432S022C_LCD_PIN_D6;
+    busCfg.pin_d7 = CYD_2432S022C_LCD_PIN_D7;
+    
+    // Bus timing
+    busCfg.freq_write = CYD_2432S022C_LCD_PCLK_HZ;  // 12MHz clock rate
+    
     bus->config(busCfg);
 
     // Apply configuration
