891e310495e3fe10e1d389e9329f777d9e4412ca
NellowTCS <neeljaiswal23@gmail.com>
2025-10-14 14:00:17 -0600

    Add support for Lilygo TDongle S3 board

--- Firmware/Source/Boards.h
+++ Firmware/Source/Boards.h
@@ -14,6 +14,9 @@
 #elif defined(CONFIG_TT_BOARD_CYD_2432S022C)
 #include "CYD2432S022C.h"
 #define TT_BOARD_HARDWARE &cyd_2432s022c_config
+#elif defined(CONFIG_TT_BOARD_LILYGO_TDONGLE_S3)
+#include "LilygoTdongleS3.h"
+#define TT_BOARD_HARDWARE &lilygo_tdongle_s3
 #elif defined(CONFIG_TT_BOARD_LILYGO_TLORA_PAGER)
 #include "LilygoTloraPager.h"
 #define TT_BOARD_HARDWARE &lilygo_tlora_pager
