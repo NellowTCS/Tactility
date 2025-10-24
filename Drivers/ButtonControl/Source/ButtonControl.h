ca691d9351552f93fb311f0c551ac16bbd56814c
NellowTCS <neeljaiswal23@gmail.com>
2025-10-24 14:56:09 -0600

    revert

--- Drivers/ButtonControl/Source/ButtonControl.h
+++ Drivers/ButtonControl/Source/ButtonControl.h
@@ -1,141 +1,162 @@
-#pragma once
+#include "ButtonControl.h"
 
-#include <Tactility/hal/encoder/EncoderDevice.h>
-#include <Tactility/hal/gpio/Gpio.h>
-#include <Tactility/TactilityCore.h>
-#include <queue>
-#include <memory>
+#include <Tactility/Log.h>
 
-class ButtonControl final : public tt::hal::encoder::EncoderDevice {
+#include <esp_lvgl_port.h>
 
-public:
+constexpr auto* TAG = "ButtonControl";
 
-    enum class Event {
-        ShortPress,
-        LongPress
-    };
+ButtonControl::ButtonControl(const std::vector<PinConfiguration>& pinConfigurations) : pinConfigurations(pinConfigurations) {
+    pinStates.resize(pinConfigurations.size());
+    for (const auto& pinConfiguration : pinConfigurations) {
+        tt::hal::gpio::configure(pinConfiguration.pin, tt::hal::gpio::Mode::Input, false, false);
+    }
+}
 
-    enum class Action {
-        UiSelectNext,
-        UiSelectPrevious,
-        UiPressSelected,
-        AppClose,
-    };
+ButtonControl::~ButtonControl() {
+    if (driverThread != nullptr && driverThread->getState() != tt::Thread::State::Stopped) {
+        interruptDriverThread = true;
+        driverThread->join();
+    }
+}
 
-    struct PinConfiguration {
-        tt::hal::gpio::Pin pin;
-        Event event;
-        Action action;
-    };
+void ButtonControl::readCallback(lv_indev_t* indev, lv_indev_data_t* data) {
+    // Defaults
+    data->enc_diff = 0;
+    data->state = LV_INDEV_STATE_RELEASED;
 
-private:
+    auto* self = static_cast<ButtonControl*>(lv_indev_get_driver_data(indev));
 
-    // Internal event structure for queueing GPIO events
-    struct GpioEvent {
-        size_t pinIndex;
-        bool pressed;
-        uint32_t timestamp;
-    };
+    if (self->mutex.lock(100)) {
 
-    // Track state of each physical pin
-    struct PinState {
-        uint32_t pressStartTime = 0;
-        bool isPressed = false;
-        bool debouncing = false;
-        uint32_t lastTransitionTime = 0;
-    };
-
-    // Track pending LVGL actions
-    struct PendingAction {
-        Action action;
-        bool needsRelease;  // For UiPressSelected which needs press+release cycle
-    };
-
-    static constexpr uint32_t DEBOUNCE_MS = 50;
-    static constexpr uint32_t LONG_PRESS_MS = 500;
-
-    lv_indev_t* _Nullable deviceHandle = nullptr;
-    std::vector<PinConfiguration> pinConfigurations;
-    std::vector<PinState> pinStates;
-    
-    // ISR-safe event queue for ISR -> main thread communication
-    // Using a simple ring buffer instead of std::queue + mutex
-    static constexpr size_t EVENT_QUEUE_SIZE = 16;
-    GpioEvent eventQueue[EVENT_QUEUE_SIZE];
-    volatile size_t queueHead = 0;
-    volatile size_t queueTail = 0;
-    
-    // Debug counter (remove after testing)
-    volatile uint32_t isrCounter = 0;
-    
-    // Pending actions to deliver to LVGL
-    std::queue<PendingAction> actionQueue;
-    bool deliveredPress = false;  // Track if we need to send release next
-
-    void handleGpioEvent(const GpioEvent& event);
-    void processEventQueue();
-    void deliverActionsToLvgl(lv_indev_data_t* data);
-    
-    // ISR-safe queue operations
-    bool queuePush(const GpioEvent& event);
-    bool queuePop(GpioEvent& event);
-    
-    static void readCallback(lv_indev_t* indev, lv_indev_data_t* data);
-
-public:
-
-    explicit ButtonControl(const std::vector<PinConfiguration>& pinConfigurations);
-    ~ButtonControl() override;
-
-    std::string getName() const override { return "ButtonControl"; }
-    std::string getDescription() const override { return "Interrupt-driven button input"; }
-
-    bool startLvgl(lv_display_t* display) override;
-    bool stopLvgl() override;
-
-    lv_indev_t* _Nullable getLvglIndev() override { return deviceHandle; }
-
-    // Factory methods
-    static std::shared_ptr<ButtonControl> createOneButtonControl(tt::hal::gpio::Pin pin) {
-        return std::make_shared<ButtonControl>(std::vector {
-            PinConfiguration {
-                .pin = pin,
-                .event = Event::ShortPress,
-                .action = Action::UiSelectNext
-            },
-            PinConfiguration {
-                .pin = pin,
-                .event = Event::LongPress,
-                .action = Action::UiPressSelected
+        for (int i = 0; i < self->pinConfigurations.size(); i++) {
+            const auto& config = self->pinConfigurations[i];
+            std::vector<PinState>::reference state = self->pinStates[i];
+            const bool trigger = (config.event == Event::ShortPress && state.triggerShortPress) ||
+                (config.event == Event::LongPress && state.triggerLongPress);
+            state.triggerShortPress = false;
+            state.triggerLongPress = false;
+            if (trigger) {
+                switch (config.action) {
+                    case Action::UiSelectNext:
+                        data->enc_diff = 1;
+                        break;
+                    case Action::UiSelectPrevious:
+                        data->enc_diff = -1;
+                        break;
+                    case Action::UiPressSelected:
+                        data->state = LV_INDEV_STATE_PRESSED;
+                        break;
+                    case Action::AppClose:
+                        // TODO: implement
+                        break;
+                }
             }
-        });
+        }
+        self->mutex.unlock();
+    }
+}
+
+void ButtonControl::updatePin(std::vector<PinConfiguration>::const_reference configuration, std::vector<PinState>::reference state) {
+    if (tt::hal::gpio::getLevel(configuration.pin)) { // if pressed
+        if (state.pressState) {
+            // check time for long press trigger
+            auto time_passed = tt::kernel::getMillis() - state.pressStartTime;
+            if (time_passed > 500) {
+                // state.triggerLongPress = true;
+            }
+        } else {
+            state.pressStartTime = tt::kernel::getMillis();
+            state.pressState = true;
+        }
+    } else { // released
+        if (state.pressState) {
+            auto time_passed = tt::kernel::getMillis() - state.pressStartTime;
+            if (time_passed < 500) {
+                TT_LOG_D(TAG, "Trigger short press");
+                state.triggerShortPress = true;
+            }
+            state.pressState = false;
+        }
+    }
+}
+
+void ButtonControl::driverThreadMain() {
+    while (!shouldInterruptDriverThread()) {
+        if (mutex.lock(100)) {
+            for (int i = 0; i < pinConfigurations.size(); i++) {
+                updatePin(pinConfigurations[i], pinStates[i]);
+            }
+            mutex.unlock();
+        }
+        tt::kernel::delayMillis(5);
+    }
+}
+
+bool ButtonControl::shouldInterruptDriverThread() const {
+    bool interrupt = false;
+    if (mutex.lock(50 / portTICK_PERIOD_MS)) {
+        interrupt = interruptDriverThread;
+        mutex.unlock();
+    }
+    return interrupt;
+}
+
+void ButtonControl::startThread() {
+    TT_LOG_I(TAG, "Start");
+
+    mutex.lock();
+
+    interruptDriverThread = false;
+
+    driverThread = std::make_shared<tt::Thread>("ButtonControl", 4096, [this] {
+        driverThreadMain();
+        return 0;
+    });
+
+    driverThread->start();
+
+    mutex.unlock();
+}
+
+void ButtonControl::stopThread() {
+    TT_LOG_I(TAG, "Stop");
+
+    mutex.lock();
+    interruptDriverThread = true;
+    mutex.unlock();
+
+    driverThread->join();
+
+    mutex.lock();
+    driverThread = nullptr;
+    mutex.unlock();
+}
+
+bool ButtonControl::startLvgl(lv_display_t* display) {
+    if (deviceHandle != nullptr) {
+        return false;
     }
 
-    static std::shared_ptr<ButtonControl> createTwoButtonControl(
-        tt::hal::gpio::Pin primaryPin, 
-        tt::hal::gpio::Pin secondaryPin
-    ) {
-        return std::make_shared<ButtonControl>(std::vector {
-            PinConfiguration {
-                .pin = primaryPin,
-                .event = Event::ShortPress,
-                .action = Action::UiPressSelected
-            },
-            PinConfiguration {
-                .pin = primaryPin,
-                .event = Event::LongPress,
-                .action = Action::AppClose
-            },
-            PinConfiguration {
-                .pin = secondaryPin,
-                .event = Event::ShortPress,
-                .action = Action::UiSelectNext
-            },
-            PinConfiguration {
-                .pin = secondaryPin,
-                .event = Event::LongPress,
-                .action = Action::UiSelectPrevious
-            }
-        });
+    startThread();
+
+    deviceHandle = lv_indev_create();
+    lv_indev_set_type(deviceHandle, LV_INDEV_TYPE_ENCODER);
+    lv_indev_set_driver_data(deviceHandle, this);
+    lv_indev_set_read_cb(deviceHandle, readCallback);
+
+    return true;
+}
+
+bool ButtonControl::stopLvgl() {
+    if (deviceHandle == nullptr) {
+        return false;
     }
-};
+
+    lv_indev_delete(deviceHandle);
+    deviceHandle = nullptr;
+
+    stopThread();
+
+    return true;
+}
