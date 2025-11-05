e8a3ee5c422b797cfa225c1ef011ef055aa1fb43
NellowTCS <neeljaiswal23@gmail.com>
2025-11-05 10:42:25 -0700

    Make timer public

--- Boards/cl-32/Source/devices/GxEPD2Display.h
+++ Boards/cl-32/Source/devices/GxEPD2Display.h
@@ -51,6 +51,9 @@ public:
                        bool invert = false, bool mirror_y = false);
     void refreshDisplay(bool partial);
 
+    // Timer callback used for debouncing partial refreshes
+    static void refreshTimerCb(void* arg);
+
 private:
     Configuration _config;
     std::unique_ptr<GxEPD2_290_GDEY029T71H> _display;
@@ -71,7 +74,4 @@ private:
     
     // Convert RGB565 pixel to monochrome (true=white, false=black)
     static inline bool rgb565ToMono(lv_color_t pixel);
-
-    // Timer callback used for debouncing partial refreshes
-    static void refreshTimerCb(void* arg);
 };
