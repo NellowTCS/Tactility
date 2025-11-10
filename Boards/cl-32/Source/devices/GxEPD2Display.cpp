7c1cc5e4b688a8a260356f1f52940efca910ddf9
NellowTCS <neeljaiswal23@gmail.com>
2025-11-10 11:17:00 -0700

    looking at something that was wrong/old

--- Boards/cl-32/Source/devices/GxEPD2Display.cpp
+++ Boards/cl-32/Source/devices/GxEPD2Display.cpp
@@ -66,10 +66,10 @@ bool GxEPD2Display::start() {
 
     // SPI device configuration for the e-paper display
     spi_device_interface_config_t dev_cfg = {
-        .clock_speed_hz = 10000000,    // SPI clock
         .mode = 0,                     // SPI mode 0
-        .spics_io_num = _config.rstPin, // Chip Select GPIO (CS pin)
+        .clock_speed_hz = 10000000,    // SPI clock
         .input_delay_ns = 0,
+        .spics_io_num = _config.rstPin, // Chip Select GPIO (CS pin)
         .queue_size = 7,               // Transactions queued at once
         .pre_cb = nullptr,
         .post_cb = nullptr,
